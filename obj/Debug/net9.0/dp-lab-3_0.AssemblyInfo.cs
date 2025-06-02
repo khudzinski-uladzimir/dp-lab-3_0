804add47c7eb892a5c67dfe5168e0fc13df7d74d
Vladimir <vladimir.hudinsky@outlook.com>
2025-06-02 19:01:19 +0200

    second commit

--- obj/Debug/net9.0/dp-lab-3_0.AssemblyInfo.cs
+++ obj/Debug/net9.0/dp-lab-3_0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dp-lab-3_0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c62c5086ca1c56e9bdb6a434ba880628110ea80")]
 [assembly: System.Reflection.AssemblyProductAttribute("dp-lab-3_0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dp-lab-3_0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
